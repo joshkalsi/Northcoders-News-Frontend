e31c93e651860efb121abeb9e963370dc0d0019f
joshkalsi <josh.kalsi@gmail.com>
2018-09-19 10:53:17 +0100

    Initial setup of React

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    user: null
+  }
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
