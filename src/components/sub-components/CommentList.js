07e40a1fd8bb26ecd4e89f6f1afed02b67ba00a5
joshkalsi <josh.kalsi@gmail.com>
2018-09-20 16:44:38 +0100

    Added comment sort functionality

--- src/components/sub-components/CommentList.js
+++ src/components/sub-components/CommentList.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import SingleComment from './SingleComment';
+import moment from 'moment';
+import _ from 'lodash';
+class CommentList extends Component {
+  state = {
+    sortOrder: 'recent'
+  }
+  render() {
+    let { sortOrder } = this.state;
+    let { comments } = this.props;
+    const sortRecent = (a, b) => {
+      if (moment(a.created_at).isBefore(moment(b.created_at))) return 1;
+      else return -1;
+    };
+    const sortVotes = (a, b) => {
+      return b.votes - a.votes;
+    };
 
-const CommentList = ({ comments }) => {
-  return (
-    <div>
-      {comments.map(comment => {
-        return <SingleComment key={comment._id} comment={comment} />;
-      })}
-    </div>
-  );
-};
+    if (sortOrder === 'recent') comments = comments.sort(sortRecent);
+    else if (sortOrder === 'votes') comments = comments.sort(sortVotes);
+    else if (sortOrder === 'random') comments = _.shuffle(comments);
+    return (
+      <div>
+        <div className="sort-order">
+          <p onClick={() => this.changeSortOrder('recent')} className="sort-recent">Recent</p>
+          <p onClick={() => this.changeSortOrder('votes')} className="sort-votes">Most Votes</p>
+        </div>
+        {comments.map(comment => {
+          return <SingleComment key={comment._id} comment={comment} />;
+        })}
+      </div>
+    );
+  }
+
+  changeSortOrder = (order) => {
+    this.setState({
+      sortOrder: order
+    });
+  }
+}
 
 CommentList.propTypes = {
   comments: PropTypes.array
